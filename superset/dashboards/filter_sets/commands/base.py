d7913f6ed697b09cf9cdc6468553b7839e64b44d
Josh Soref <jsoref@users.noreply.github.com>
2022-04-10 17:51:38 -0400

    spelling: authorized
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- superset/dashboards/filter_sets/commands/base.py
+++ superset/dashboards/filter_sets/commands/base.py
@@ -20,7 +20,7 @@ from typing import cast, Optional
 from flask_appbuilder.models.sqla import Model
 from flask_appbuilder.security.sqla.models import User
 
-from superset.common.not_authrized_object import NotAuthorizedException
+from superset.common.not_authorized_object import NotAuthorizedException
 from superset.common.request_contexed_based import is_user_admin
 from superset.dashboards.commands.exceptions import DashboardNotFoundError
 from superset.dashboards.dao import DashboardDAO
